3c9d1e2a585d6caddba1b44209fff76140384597
AlexMorales <admoralesp@uce.edu.ec>
2025-07-04 11:58:19 -0500

    feat: :fire: fix element

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import type { JSX } from "react";
 
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
   const { token } = useAuth();
