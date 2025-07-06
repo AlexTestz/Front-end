e3aba8f01ed12d60164e79501433aa8b9f526158
AlexMorales <admoralesp@uce.edu.ec>
2025-07-06 18:21:52 -0500

    feat: :bug: get names

--- src/pages/EditServicePage.tsx
+++ src/pages/EditServicePage.tsx
@@ -20,7 +20,7 @@ export default function EditServicePage() {
   useEffect(() => {
     const fetchService = async () => {
       try {
-        const res = await axios.get(`http://3.211.68.117:8000/api/services/${id}`);
+        const res = await axios.get(`http://3.211.68.117:8000/api/services/name/${id}`);
         setService(res.data); // Asegúrate de que la respuesta contiene los datos correctamente
       } catch (err) {
         setErrorMsg("❌ Error loading service data.");
