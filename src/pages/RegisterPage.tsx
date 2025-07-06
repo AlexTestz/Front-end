65463f6b0f46998b99fc1bfc5c035d8dc12e138c
AlexMorales <admoralesp@uce.edu.ec>
2025-07-06 13:55:53 -0500

    rol change

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -21,7 +21,7 @@ export default function RegisterPage() {
       username,
       email,
       password,
-      role: "admin", // Assuming the role is always 'user' for registration
+      role: "usuario", // Assuming the role is always 'user' for registration
     };
 
     try {
