dc8248d23701d20d175e008eed847ce51c0c4704
AlexMorales <admoralesp@uce.edu.ec>
2025-07-06 12:22:29 -0500

    fix css

--- src/pages/ClientListPage.tsx
+++ src/pages/ClientListPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./ClientsList.css"; // Import your CSS styles
+import "../components/ClientsList.css"; // Import your CSS styles
 
 export default function ClientsListPage() {
   const [searchTerm, setSearchTerm] = useState("");
